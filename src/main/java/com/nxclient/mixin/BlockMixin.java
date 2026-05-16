be1db121b77db93aad8fb519a33696bc186e7de3
dilekarkun6 <nxclient@github.com>
2026-05-16 19:18:12 +0000

    Fix BlockMixin signature for 1.21.4 shouldDrawSide(BlockState, BlockState, Direction)

--- src/main/java/com/nxclient/mixin/BlockMixin.java
+++ src/main/java/com/nxclient/mixin/BlockMixin.java
@@ -3,9 +3,7 @@ package com.nxclient.mixin;
 import com.nxclient.modules.render.XRay;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class BlockMixin {
 
     @Inject(method = "shouldDrawSide", at = @At("HEAD"), cancellable = true)
-    private static void onShouldDrawSide(BlockState state, BlockView view, BlockPos pos, Direction direction, BlockPos sidePos, CallbackInfoReturnable<Boolean> cir) {
+    private static void onShouldDrawSide(BlockState state, BlockState otherState, Direction side, CallbackInfoReturnable<Boolean> cir) {
         if (XRay.active && !XRay.isOre(state.getBlock())) {
             cir.setReturnValue(false);
         }
